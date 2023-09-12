34e59bc2b9c7e459b8360b4a06f761b9b62d758c
Mert Ulunehir <mertulunehir@gmail.com>
2023-09-12 23:10:38 +0300

    feat : reward selection part : 1

--- Assets/Scripts/RewardsDatabase.cs
+++ Assets/Scripts/RewardsDatabase.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class RewardsDatabase : MonoBehaviour
 {
@@ -11,13 +12,61 @@ public class RewardsDatabase : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        List<Rewards> rewards = GetRandomElementsForLuckyWheel(1);
+        foreach (Rewards item in rewards)
+        {
+            Debug.Log(item.itemSO.itemName);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public List<Rewards> GetRandomElementsForLuckyWheel(int zoneNumber)
+    {
+        if(zoneNumber<5)
+        {
+            return GetRandomElements(firstTierRewards);
+        }
+        else if (zoneNumber == 5)
+        {
+            return GetRandomElements(firstTierRewards);
+        }
+        else if(zoneNumber<15)
+        {
+            return GetRandomElements(secondTierRewards);
+
+        }
+        else if(zoneNumber == 30)
+        {
+            return GetRandomElements(thirdTierRewards);
+        }
+        else
+        {
+            return GetRandomElements(thirdTierRewards);
+
+        }
+    }
+
+    private List<Rewards> GetRandomElements(List<Rewards> currentRewards)
+    {
+        Random random = new Random();
+
+        List<Rewards> randomSelection = new List<Rewards>();
+
+        for (int i = 0; i < 8; i++)
+        {
+            int randomIndex = random.Next(currentRewards.Count); 
+            Rewards selectedElement = currentRewards[randomIndex]; 
+            randomSelection.Add(selectedElement); 
+        }
+
+        return randomSelection;
+
     }
 }
 
