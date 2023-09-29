331ff8f7c7f0eee760cad2eeb14860fc619ce18b
Mert Ulunehir <mertulunehir@gmail.com>
2023-09-29 21:26:50 +0300

    feat : button spin anim integrated

--- Assets/_Main/Scripts/LuckyWheelScene/LuckyWheelButtonController.cs
+++ Assets/_Main/Scripts/LuckyWheelScene/LuckyWheelButtonController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
+using DG.Tweening;
+using System;
 
 namespace LWheel.LuckyWheelNameSpace
 {
@@ -12,11 +14,20 @@ namespace LWheel.LuckyWheelNameSpace
         [SerializeField] private Button rotateButton;
 
         private bool canPressButton;
-        [Inject]private LuckyWheel luckyWheel;
+        private Tween spinAnimTween;
+        private Tween sizeAnimTween;
+
+        [Inject] private LuckyWheel luckyWheel;
+
+        public float buttonRotateTime = 1;
+        public float buttonSpinAnimDelay = 1;
+        public Ease rotateEase = Ease.OutBack;
 
         private void Start()
         {
             canPressButton = true;
+            StartButtonRotateAnim();
+
         }
         private void OnRotateButtonPressed()
         {
@@ -24,12 +35,65 @@ namespace LWheel.LuckyWheelNameSpace
             {
                 canPressButton = false;
                 luckyWheel.ChooseRewardAfterButtonClick();
+                StopButtonRotateAnim();
+                RotateAfterButtonClick();
+                GrowAfterButtonClick();
             }
         }
 
         public void EnableButton()
         {
+            StopButtonRotateAnim();
             canPressButton = true;
+            StartButtonRotateAnim();
+        }
+
+
+        private void StartButtonRotateAnim()
+        {
+            if (canPressButton == true)
+            {
+                DOVirtual.DelayedCall(buttonSpinAnimDelay, RotateButton);
+            }
+        }
+
+        private void RotateButton()
+        {
+            if(canPressButton==true)
+            {
+                sizeAnimTween = rotateButton.transform.DOScale(Vector3.one * 1.1f, buttonRotateTime * 0.3f).OnComplete(ButtonShrinkAnim);
+                spinAnimTween = rotateButton.transform.DORotate(new Vector3(0, 0, 360), buttonRotateTime, RotateMode.LocalAxisAdd).SetEase(rotateEase).OnComplete(StartButtonRotateAnim);
+
+            }
+        }
+
+        private void RotateAfterButtonClick()
+        {
+            spinAnimTween.Kill();
+            rotateButton.transform.localEulerAngles = Vector3.zero;
+            spinAnimTween = rotateButton.transform.DORotate(new Vector3(0, 0, 360), buttonRotateTime, RotateMode.LocalAxisAdd).SetEase(rotateEase).OnComplete(StartButtonRotateAnim);
+
+        }
+
+        private void GrowAfterButtonClick()
+        {
+            sizeAnimTween = rotateButton.transform.DOScale(Vector3.one * 1.3f, buttonRotateTime * 0.3f).OnComplete(ButtonShrinkAnim);
+        }
+
+        private void ButtonShrinkAnim()
+        {
+            sizeAnimTween = rotateButton.transform.DOScale(Vector3.one, buttonRotateTime * 0.7f);
+        }
+
+        private void StopButtonRotateAnim()
+        {
+
+            spinAnimTween.Kill();
+            rotateButton.transform.localEulerAngles = Vector3.zero;
+
+            sizeAnimTween.Kill();
+            rotateButton.transform.localScale = Vector3.one;
+
         }
 
         private void OnEnable()
@@ -40,6 +104,8 @@ namespace LWheel.LuckyWheelNameSpace
         private void OnDisable()
         {
             rotateButton.onClick.RemoveListener(OnRotateButtonPressed);
+
         }
+
     }
 }
